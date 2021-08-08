16bcb4d6fe5a2ce673570a0c3ef1417e39b2b9df
Develeopard <frontendwebdeveleopard@gmail.com>
2021-08-08 20:15:43 +0200

    files edited

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <h1>reaction timer</h1>
   <button @click="start" :disabled="isPlaying" >play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score"  />
 </template>
 
 <script>
-import { computed, ref } from '@vue/runtime-core'
+import { ref } from '@vue/runtime-core'
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components:{
-    Block
+    Block,
+    Results
   },
   setup(){
     const isPlaying = ref(false)
@@ -20,11 +22,12 @@ export default {
     const score = ref(null)
     const showResults = ref(false)
 
-    const start = computed(() => {
+    function start(){
       delay.value = 2000 + Math.random() * 5000
       isPlaying.value = true
       console.log(delay.value);
-    })
+      showResults.value = false
+    }
 
     function endGame(reactionTime){
       score.value = reactionTime
---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>Reaction time: {{ score }} ms</h2>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import { onMounted } from '@vue/runtime-core'
+export default {
+    props: ['score'],
+    setup(props){
+        const rank = ref(null)
+
+        onMounted(() => {
+            if(props.score < 250){
+                rank.value = 'Fast'
+            }else if(props.score < 400){
+                rank.value = 'Moderate'
+            }else{
+                rank.value = 'Slow'
+            }
+
+            console.log('rank mounted');
+        })
+
+        return{
+            rank,
+        }
+    }
+    }
+</script>
+
+<style>
+
+</style>
